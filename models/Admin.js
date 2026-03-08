7667c9c74d6cad9f0e081a804b902d9295e43f9a
vinudasenith <ha.vinudas@gmail.com>
2026-03-08 07:04:40 +0530

    feat: add Admin model

---
+++ models/Admin.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+
+        lastName: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        role: {
+            type: String,
+            default: "super_admin"
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Admin", adminSchema);
